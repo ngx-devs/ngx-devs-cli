5f72290fd066d580c621ddd55aa69aeeccb8d323
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-04-14 02:08:45 -0300

    :bug: fix: read of package.json

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,16 +1,16 @@
 import { GluegunCommand, GluegunToolbox } from 'gluegun';
 import { PackageJSON } from 'gluegun/build/types/toolbox/meta-types';
 
-import { printCreated } from '../utils/functions.helper';
+import { printVersion } from '../utils/functions.helper';
 
 const COMMAND: GluegunCommand = {
   name: 'version',
   alias: ['v', '-v', '--version', '-version'],
   description: 'Cria um componente Angular de tipo Page',
   run: async ({ print }: GluegunToolbox) => {
-    const packageJson: PackageJSON = require('../package.json');
+    const packageJson: PackageJSON = require('../../package.json');
     const version = packageJson?.version;
-    printCreated(print, version);
+    printVersion(print, version);
   }
 };
 
