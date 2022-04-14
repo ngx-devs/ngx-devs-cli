0483c6280a7f09797fa3ee3923651a76d77c0902
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-04-14 02:08:18 -0300

    :package: refactor: output version

--- src/commands/ngx-devs-cli.ts
+++ src/commands/ngx-devs-cli.ts
@@ -1,29 +1,30 @@
 import { GluegunCommand } from 'gluegun';
 
 import { ASCII_ART } from '../utils/cli-ascii.const';
+import { printVersion } from '../utils/functions.helper';
 
 const COMMAND: GluegunCommand = {
   name: 'ngx-devs-cli',
   alias: ['ngxd'],
   description: 'Inicializa a CLI do NgxDevs',
   run: async (toolbox) => {
-    const { print } = toolbox
-    const version = require('../../package.json').version
+    const { print } = toolbox;
+    const version = require('../../package.json').version;
 
-    print.info(ASCII_ART)
-    print.info(`  version: ${version}`)
-    print.divider()
+    print.info(ASCII_ART);
+    printVersion(print, version);
+    print.divider();
 
-    print.highlight('Como Utilizar:')
-    print.newline()
+    print.highlight('Como Utilizar:');
+    print.newline();
 
-    print.info('  ngxd <command>')
-    print.info('  ngxd <command> [options]')
-    print.divider()
+    print.info('  ngxd <command>');
+    print.info('  ngxd <command> [options]');
+    print.divider();
 
-    print.highlight('Comandos:')
-    print.printCommands(toolbox)
-  },
-}
+    print.highlight('Comandos:');
+    print.printCommands(toolbox);
+  }
+};
 
-module.exports = COMMAND
+module.exports = COMMAND;
