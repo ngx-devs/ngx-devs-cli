ba522f1dda0a11c24b9a394e811868d9f3c0c01b
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-04-14 02:01:31 -0300

    :package: refactor: change version to custom command

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,16 +1,16 @@
 import { GluegunCommand, GluegunToolbox } from 'gluegun';
 import { PackageJSON } from 'gluegun/build/types/toolbox/meta-types';
 
-import { printVersion } from '../utils/functions.helper';
+import { printCreated } from '../utils/functions.helper';
 
 const COMMAND: GluegunCommand = {
   name: 'version',
   alias: ['v', '-v', '--version', '-version'],
   description: 'Cria um componente Angular de tipo Page',
   run: async ({ print }: GluegunToolbox) => {
-    const packageJson: PackageJSON = require('../../package.json');
+    const packageJson: PackageJSON = require('../package.json');
     const version = packageJson?.version;
-    printVersion(print, version);
+    printCreated(print, version);
   }
 };
 
