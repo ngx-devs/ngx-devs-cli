0dd2c0dcd08ea99db156793e2ec843d537ae5b83
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-04-20 14:37:56 -0300

    :test_tube: test: merge test files

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -15,4 +15,75 @@ describe('[CLI]', () => {
       expect(output).toContain(version);
     });
   });
+
+  describe('[Commands: generate page component]', () => {
+    afterEach(() => {
+      filesystem.remove('sample');
+    });
+
+    test('should generate a page component with 3 files', async () => {
+      await cli('g c p sample');
+
+      const html = filesystem.read('sample/sample.page.html');
+      const scss = filesystem.read('sample/sample.page.scss');
+      const ts = filesystem.read('sample/sample.page.ts');
+
+      expect(html).toBeDefined();
+      expect(scss).toBeDefined();
+      expect(ts).toBeDefined();
+    });
+
+    test('should generate a page component html with default template <p>sample works</p>', async () => {
+      await cli('g c p sample');
+
+      const html = filesystem.read('sample/sample.page.html');
+      expect(html).toContain('<p>sample works</p>');
+    });
+
+    test('should generate a page component with correct templateUrl: and styleUrls ', async () => {
+      await cli('g c p sample');
+
+      const ts = filesystem.read('sample/sample.page.ts');
+
+      expect(ts).toContain(`templateUrl: './sample.page.html'`);
+      expect(ts).toContain(`styleUrls: ['./sample.page.scss']`);
+    });
+  });
+
+  describe('[Commands: generate widget component]', () => {
+    afterEach(() => {
+      filesystem.remove('sample');
+    });
+
+    test('should generate widget component with 4 files', async () => {
+      await cli('g c w sample');
+
+      const html = filesystem.read('sample/sample.component.html');
+      const scss = filesystem.read('sample/sample.component.scss');
+      const ts = filesystem.read('sample/sample.component.ts');
+      const widgetModule = filesystem.read('sample/sample.widget.module.ts');
+
+      expect(html).toBeDefined();
+      expect(scss).toBeDefined();
+      expect(ts).toBeDefined();
+      expect(widgetModule).toBeDefined();
+    });
+
+    test('should generate widget component html with default template <p>sample works</p>', async () => {
+      await cli('g c w sample');
+
+      const html = filesystem.read('sample/sample.component.html');
+
+      expect(html).toContain('<p>sample works</p>');
+    });
+
+    test('should generate a widget component with correct templateUrl: and styleUrls ', async () => {
+      await cli('g c w sample');
+
+      const ts = filesystem.read('sample/sample.component.ts');
+
+      expect(ts).toContain(`templateUrl: './sample.component.html'`);
+      expect(ts).toContain(`styleUrls: ['./sample.component.scss']`);
+    });
+  });
 });
