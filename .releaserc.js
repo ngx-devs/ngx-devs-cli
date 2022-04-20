b588a5b31774e58ded214d7684ae24b65425bb30
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-04-20 02:54:43 -0300

    :bug: fix: how to read hbs template

--- .releaserc.js
+++ .releaserc.js
@@ -1,15 +1,8 @@
 const path = require('path');
-const { promisify } = require('util');
+const fs = require('fs');
 const dateFormat = require('dateformat');
-const readFileAsync = promisify(require('fs').readFile);
 
-// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
-
-// the *.hbs template and partials should be passed as strings of contents
-const template = readFileAsync(path.join(TEMPLATE_DIR, 'release-notes.hbs'));
-const commitTemplate = readFileAsync(
-  path.join(TEMPLATE_DIR, 'commit-template.hbs')
-);
+const template = path.resolve(__dirname, 'release-notes.hbs');
 
 module.exports = {
   plugins: [
@@ -19,11 +12,10 @@ module.exports = {
         releaseRules: {
           major: [':boom:'],
           minor: [':sparkles:'],
-          patch: [':bug:', ':ambulance:', ':lock:']
+          patch: [':bug:', ':package:']
         },
         releaseNotes: {
-          template,
-          partials: { commitTemplate },
+          template: fs.readFileSync(template, 'utf8'),
           helpers: {
             datetime: function (format = 'UTC:yyyy-mm-dd') {
               return dateFormat(new Date(), format);
