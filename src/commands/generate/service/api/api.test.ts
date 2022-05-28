80e01b464f04206d0f6cce872f1974f5d9dacb7f
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-05-28 05:24:29 -0300

    :test_tube: test(commands): test cases for api and common service

---
+++ src/commands/generate/service/api/api.test.ts
@@ -0,0 +1,47 @@
+import { filesystem } from 'gluegun';
+
+import { runNgxdCLI } from '../../../../utils/cli-test-setup';
+
+describe('Commands: [Generate] => [Service] => [Api]', () => {
+  const name = 'gsc';
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setTimeout(100000);
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  test('should generate a api service with 2 files', async () => {
+    await runNgxdCLI(`g s a ${name}`);
+
+    const ts = filesystem.read(`${name}/${name}.api.ts`);
+    const spec = filesystem.read(`${name}/${name}.api.spec.ts`);
+
+    expect(ts).toBeDefined();
+    expect(spec).toBeDefined();
+    filesystem.remove(`${name}`);
+  });
+
+  test('should generate a api service with correct content ', async () => {
+    const name = 'fruit';
+
+    await runNgxdCLI(`g s a ${name}`);
+
+    const ts = filesystem.read(`${name}/${name}.api.ts`);
+    const spec = filesystem.read(`${name}/${name}.api.spec.ts`);
+
+    expect(ts).toContain(`import { Injectable } from '@angular/core'`);
+    expect(ts).toContain(`@Injectable({`);
+    expect(ts).toContain(`providedIn: 'root'`);
+    expect(ts).toContain(`export class FruitApi {`);
+
+    expect(spec).toContain("describe('FruitApi', () => {");
+    expect(spec).toContain("it('should be created', () => {");
+    expect(spec).toContain('service = TestBed.inject(FruitApi);');
+
+    filesystem.remove(`${name}`);
+  });
+});
